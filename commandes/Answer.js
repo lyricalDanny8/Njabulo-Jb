3bddf4c71b62e4e387105a016c33fcfe5f3e7af1
NjabuloJ <fanajb65@gmail.com>
2024-12-27 13:31:29 +0300

    Update Answer.js

--- commandes/Answer.js
+++ commandes/Answer.js
@@ -11,7 +11,7 @@ zokou({nomCom:"Njabulo",reaction:"📡",categorie:"IA"},async(dest,zk,commandeOp
   const {repondre,ms,arg}=commandeOptions;
   
     if(!arg || !arg[0])
-    {return repondre("*Hey Pal How Can i Be of Help*.")}
+    {return repondre("*Hey Pal.... Njabulo here How may i help you*")}
     //var quest = arg.join(' ');
   try{
     
@@ -58,7 +58,7 @@ fetch(`http://api.brainshop.ai/get?bid=177607&key=NwzhALqeO1kubFVD&uid=[uid]&msg
       const response = await axios.get(`http://api.maher-zubair.tech/ai/photoleap?q=${image}`);
       
       const data = response.data;
-      let caption = '*powered by Njabulo JB*';
+      let caption = '*powered by BERA TECH*';
       
       if (data.status == 200) {
         // Utiliser les données retournées par le service
@@ -98,7 +98,7 @@ fetch(`http://api.brainshop.ai/get?bid=177607&key=NwzhALqeO1kubFVD&uid=[uid]&msg
   });
 
 
-zokou({ nomCom: "kiprotich", reaction: "🤔", categorie: "IA" }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "gpt", reaction: "🤔", categorie: "IA" }, async (dest, zk, commandeOptions) => {
     const { repondre, arg, ms } = commandeOptions;
   
     try {
